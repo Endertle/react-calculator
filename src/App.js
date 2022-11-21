79526b0712b536c14d5c518210838c1197dae302
Kenneth Alina <nethboy557@gmail.com>
2022-11-21 10:18:09 +0800

    calculator structure built

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calcu-grid">
+        <div className="output top-border">
+          <div className="prev-operand">32443 *</div>
+          <div className="curr-operand">4543</div>
+        </div>
+        <button className="btn btn-2 btn-method">CE</button>
+        <button className="btn btn-method">DEL</button>
+        <button className="btn btn-operator">+</button>
+        <button className="btn">7</button>
+        <button className="btn">8</button>
+        <button className="btn">9</button>
+        <button className="btn btn-operator">-</button>
+        <button className="btn">4</button>
+        <button className="btn">5</button>
+        <button className="btn">6</button>
+        <button className="btn btn-operator">x</button>
+        <button className="btn">1</button>
+        <button className="btn">2</button>
+        <button className="btn">3</button>
+        <button className="btn btn-operator">÷</button>
+        <button className="btn btn-2 bottom-left-border">0</button>
+        <button className="btn">.</button>
+        <button className="btn bottom-right-border btn-equal">=</button>
+      </div>
     </div>
   );
 }
